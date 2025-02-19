bb703f30c4348a4730e151ffedbca2a8c1b8febc
Michael Bragg <michael@michaelbragg.com>
2025-02-19 16:06:48 +0000

    Added: Improved post meta layout

--- patterns/hidden-post-meta.php
+++ patterns/hidden-post-meta.php
@@ -9,32 +9,31 @@
  * @author    Vatu <hello@vatu.dev>
  * @link      https://vatu.dev/
  * @license   GNU General Public License v3 or later
- * @copyright 2022-2024 Vatu Limited.
+ * @copyright 2022-2025 Vatu Limited.
  */
 
 declare(strict_types=1);
 
 ?>
 
-<!-- wp:group {"layout":{"type":"constrained"}, "className":"c-post-meta} -->
-<div class="wp-block-group c-post-meta">
+<!-- wp:group {"className":"c-post-meta","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group c-post-meta"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:avatar {"size":56} /-->
 
-	<!-- wp:group {"style":{"spacing":{"blockGap":"0.3em"}},"layout":{"type":"flex","justifyContent":"left"}} -->
-	<div class="wp-block-group">
+<!-- wp:group {"className":"c-post-meta__details","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group c-post-meta__details"><!-- wp:post-author {"showAvatar":false,"showBio":false,"isLink":true} /-->
 
-		<!-- wp:paragraph -->
-		<p><?php echo esc_html_x( 'Author:', 'Prefix for the post author block: By author name', 'base-theme' ); ?></p>
-		<!-- /wp:paragraph -->
+<!-- wp:group {"className":"c-post-meta__info","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group c-post-meta__info"><!-- wp:post-date {"format":"<?php echo get_option( 'date_format' ); ?>","isLink":false} /-->
 
-		<!-- wp:post-author-name {"isLink":false} /-->
+<!-- wp:paragraph -->
+<p>•</p>
+<!-- /wp:paragraph -->
 
-		<!-- wp:paragraph -->
-		<p> • </p>
-		<!-- /wp:paragraph -->
-
-		<!-- wp:post-date {"format":"<?php echo get_option( 'date_format' ); ?>","isLink":false} /-->
-
-	</div>
-	<!-- /wp:group -->
-</div>
+<!-- wp:paragraph -->
+<p>5 min read</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
