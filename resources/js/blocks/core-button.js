e3e466ca31e81f1dee5d3a9409b3f407e5c374fc
Michael Bragg <michael@michaelbragg.com>
2025-07-17 07:12:12 +0000

    Added: Toggle setting for icon position

--- resources/js/blocks/core-button.js
+++ resources/js/blocks/core-button.js
@@ -7,7 +7,12 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, PanelRow, ToggleControl, __experimentalToggleGroupControl as ToggleGroupControl, __experimentalToggleGroupControlOptionIcon as ToggleGroupControlOptionIcon } from '@wordpress/components';
+import { justifyLeft, justifyRight } from '@wordpress/icons';
 import { addFilter } from '@wordpress/hooks';
+
+
 
 /**
  * Add the attributes needed for button icons.
@@ -25,9 +30,10 @@ function addAttributes( settings ) {
 		icon: {
 			type: 'string',
 		},
-		iconPositionStart: {
-			type: 'boolean',
-			default: true,
+		iconPosition: {
+			type: 'enum',
+			default: 'start',
+			enum: [ 'start', 'end' ],
 		},
 		hideText: {
 			type: 'boolean',
@@ -48,6 +54,73 @@ function addAttributes( settings ) {
 
 addFilter(
 	'blocks.registerBlockType',
-	'vatu/add-attributes',
+	'core-button/add-attributes',
 	addAttributes
+);
+
+function addInspectorControls( BlockEdit ) {
+	return ( props ) => {
+		const { name, attributes, setAttributes } = props;
+
+		if ( name !== 'core/button' ) {
+			return <BlockEdit { ...props } />;
+		}
+
+		// Retrieve selected attributes from the block.
+		const { icon, iconPosition, hideText } = attributes;
+
+		return (
+			<>
+				<BlockEdit { ...props } />
+				<InspectorControls>
+					<PanelBody
+						title={ __(
+							'Icon',
+							'base-theme'
+						) }
+					>
+						<PanelRow>
+							<ToggleControl
+								label={ __( 'Hide text', 'base-theme' ) }
+								checked={ hideText }
+								onChange={ () => {
+									setAttributes( {
+										hideText: ! hideText,
+									} );
+								} }
+							/>
+						</PanelRow>
+						<PanelRow>
+							<ToggleGroupControl
+								__next40pxDefaultSize
+								__nextHasNoMarginBottom
+								isBlock
+								label={ __( 'Icon position', 'base-theme' ) }
+								onChange={ (value) => {
+									setAttributes( { iconPosition: value } );
+								} }
+							>
+								<ToggleGroupControlOptionIcon
+									value="start"
+									icon={ justifyLeft }
+									label={ __( 'Prepend', 'base-theme' ) }
+								/>
+								<ToggleGroupControlOptionIcon
+									value="end"
+									icon={ justifyRight }
+									label={ __( 'Append', 'base-theme' ) }
+								/>
+							</ToggleGroupControl>
+						</PanelRow>
+					</PanelBody>
+				</InspectorControls>
+			</>
+		);
+	};
+}
+
+addFilter(
+	'editor.BlockEdit',
+	'core-button/add-inspector-controls',
+	addInspectorControls
 );
