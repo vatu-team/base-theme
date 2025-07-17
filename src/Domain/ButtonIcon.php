12a3a8374a9386cede71585e2c59ec048d85a60e
Michael Bragg <michael@michaelbragg.com>
2025-07-17 08:12:50 +0000

    Added: HTML manipulation for buttons

--- src/Domain/ButtonIcon.php
+++ src/Domain/ButtonIcon.php
@@ -29,6 +29,13 @@ final class ButtonIcon extends Service implements Registrable
 			priority: 10,
 			accepted_args: 0
 		);
+
+		\add_filter(
+			hook_name: 'render_block_core/button',
+			callback: [ $this, 'renderCoreButton' ],
+			priority: 10,
+			accepted_args: 2
+		);
 	}
 
 	public function extendCoreButton(): void
@@ -41,5 +48,56 @@ final class ButtonIcon extends Service implements Registrable
 			deps: $script['dependencies'],
 			ver: $script['version']
 		);
+	}
+
+	/**
+	 * @param array<mixed> $block
+	 */
+	public function renderCoreButton( string $block_content, array $block ): string
+	{
+
+		// if ( ! isset( $block['attrs']['icon'] ) ) {
+		// 	return $block_content;
+		// }
+
+		$icon      = $block['attrs']['icon'];
+		$position  = $block['attrs']['iconPosition'];
+		$hide_text = $block['attrs']['hideText'];
+
+
+		// Append the icon class to the block.
+		$processer = new \WP_HTML_Tag_Processor( html: $block_content );
+
+		if ( $processer->next_tag() ) {
+			$processer->add_class( class_name: 'has-icon' );
+		}
+
+		$result = $processer->get_updated_html();
+
+		$icon_html = sprintf(
+			'<span class="c-icon"><svg aria-hidden="%2$s"><use xlink:href="%1$s"></use></svg></span>',
+			esc_url( get_stylesheet_directory_uri() . "/assets/svg/icons.svg#{$icon}" ),
+			'true'
+		);
+
+		$hide_html = $hide_text ? ' screen-reader-text' : null;
+
+		$result = $position
+			? preg_replace(
+				'/(<a[^>]*>)(.*?)(<\/a>)/i',
+				"$1{$icon_html}" . '<span class="wp-block-button__label' . $hide_html . '">$2</span>$3',
+				$result
+			)
+			: preg_replace(
+				'/(<a[^>]*>)(.*?)(<\/a>)/i',
+				'$1<span class="wp-block-button__label' . $hide_html . '">$2</span>' . $icon_html . '$3',
+				$result
+			);
+
+		if ( ! is_string( $result ) ) {
+			return $block_content;
+		}
+
+		return $result;
 	}
 }
